cd24f458c5cfbb2a9cabf7721e5b5f9094f4bcd4
YevheniiaRi <9263070@gmail.com>
2023-04-06 14:28:21 +0300

    HW

---
+++ src/js/task-01.js
@@ -0,0 +1,17 @@
+const NumberOfCategories =
+    document.querySelectorAll(".item");
+console.log(`Number of categories: ${NumberOfCategories.length}`);
+
+
+
+const categories = [...document.querySelectorAll('#categories .item')];
+categories.forEach((category) => {
+    const categoryName = category.querySelector('h2').textContent;
+    const categoryElements = category.querySelectorAll('li').length;
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${ categoryElements}`);
+});
+
+
+
+
