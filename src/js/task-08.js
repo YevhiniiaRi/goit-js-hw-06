21fccefdad390b78a0c425f30364d630afd3fa3e
YevheniiaRi <9263070@gmail.com>
2023-04-16 13:21:44 +0300

    HW-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
 
+
+
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+    
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (emailInput.value === '' || passwordInput.value === '') {
+    alert('Всі поля повинні бути заповнені!');
+  } else {
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value
+    };
+
+    console.log(formData);
+    loginForm.reset(); 
+  }
+});
+
+
