ebf23b186ed666cbcc7e629949d1d607ce3fcd60
YevheniiaRi <9263070@gmail.com>
2023-04-17 15:02:08 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,36 +7,13 @@ const ingredients = [
   'Condiments',
 ];
 
-const list = document.querySelector('#ingredients');
+const ul = document.querySelector("#ingredients"); 
 
-const firstItem =
-  document.createElement('li');
-firstItem.textContent = 'Potatoes';
-firstItem.classList.add("item");
+const liElements = ingredients.map((ingredient) => {
+  const li = document.createElement("li"); 
+  li.textContent = ingredient; 
+  li.classList.add("item"); 
+  return li; 
+});
 
-const secondItem =
-  document.createElement("li");
-secondItem.textContent = "Mushrooms";
-secondItem.classList.add("item");
-
-const thirdItem =
-  document.createElement("li");
-thirdItem.textContent = "Garlic";
-thirdItem.classList.add("item");
-
-const fourthItem =
-  document.createElement("li");
-fourthItem.textContent = "Tomatos";
-fourthItem.classList.add("item");
-
-const fifthItem =
-  document.createElement("li");
-fifthItem.textContent = "Herbs";
-fifthItem.classList.add("item");
-
-const sixthItem = document.createElement("li");
-sixthItem.textContent = "Condiments";
-sixthItem.classList.add("item");
-
-list.append(firstItem, secondItem, thirdItem, fourthItem, fifthItem, sixthItem);
-
+ul.append(...liElements);
